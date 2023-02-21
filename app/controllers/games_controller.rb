0ccf7d2f7c0e5e44ffdc82b248c6c99684ec9b7d
Ricardo Guerra <RikkuG491@gmail.com>
2023-02-21 18:36:19 -0500

    Added Lecture #1 content

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,28 +1,49 @@
 class GamesController < ApplicationController
     def index
         games = Game.all
-        render json: games
+        render json: games, except: [:created_at, :updated_at], status: :ok
     end
 
     def show
         game = Game.find_by(id: params[:id])
-        render json: game
+        if(game != nil)
+            render json: game, include: [:reviews, :users], status: :ok
+        else
+            render json: { error: "Game Not Found" }, status: :not_found
+        end
     end
 
     def create
         new_game = Game.create(title: params[:title], release_year: params[:release_year])
-        render json: new_game
+        render json: new_game, status: :created
     end
 
     def update
         game = Game.find_by(id: params[:id])
-        game.update(params.permit(:title, :release_year))
-        render json: game
+        if(game != nil)
+            game.update(params.permit(:title, :release_year))
+            render json: game, status: :ok
+        else
+            render json: { error: "Game Not Found" }
+        end
     end
 
     def destroy
         game = Game.find_by(id: params[:id])
-        game.destroy
-        render json: {}
+        if(game != nil)
+            game.destroy
+            render json: {}, status: :no_content
+        else
+            render json: { error: "Game Not Found" }, status: :not_found
+        end
+    end
+
+    def last_game
+        last_game = Game.all.last
+        if(Game.all.length > 0)
+            render json: last_game
+        else
+            render json: { error: "Sorry, there are no games" }
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resources :reviews
   resources :users
+  
   resources :games
+  # get "games", to: "games#index"
+  # get "games/:id", to: "games#show"
+  # post "games", to: "games#create"
+  # patch "games/:id", to: "games#update"
+  # delete "games/:id", to: "games#destroy"
+  
+  get "get_last_game", to: "games#last_game"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
