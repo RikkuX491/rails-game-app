e9d36e22197b2bff5c4387e7e690bca5210ddd96
Ricardo Guerra <RikkuG491@gmail.com>
2023-02-21 10:40:49 -0500

    Added code for reviews_controller and users_controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+    def index
+        reviews = Review.all
+        render json: reviews
+    end
+
+    def show
+        review = Review.find_by(id: params[:id])
+        render json: review
+    end
+
+    def create
+        new_review = Review.create(game_id: params[:game_id], user_id: params[:user_id], rating: params[:rating])
+        render json: new_review
+    end
+
+    def update
+        review = Review.find_by(id: params[:id])
+        review.update(params.permit(:game_id, :user_id, :rating))
+        render json: review
+    end
+
+    def destroy
+        review = Review.find_by(id: params[:id])
+        review.destroy
+        render json: {}
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        render json: user
+    end
+
+    def create
+        new_user = User.create(name: params[:name])
+        render json: new_user
+    end
+
+    def update
+        user = User.find_by(id: params[:id])
+        user.update(params.permit(:name))
+        render json: user
+    end
+
+    def destroy
+        user = User.find_by(id: params[:id])
+        user.destroy
+        render json: {}
+    end
 end
