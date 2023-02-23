847ad7cf239a6dd43d4fe55fa56aa24057ab77db
Ricardo Guerra <RikkuG491@gmail.com>
2023-02-23 06:47:38 -0500

    Added validation deliverables for Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,12 @@
 class Game < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+
+    # Validations deliverables - Add validations to this model such that...
+    # 1) All games have a title (cannot be empty / blank)
+    # 2) No two games have the same title
+    # 3) The game title is at least 4 characters long
+    # 4) The release_year is between 1900 and 2023
+
+    
 end
